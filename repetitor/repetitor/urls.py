3fb08950c3a68b56c6c79bbd9b3af8de743e3fae
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-11-05 20:54:27 +0300

    add def info

--- repetitor/repetitor/urls.py
+++ repetitor/repetitor/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index)
+    path('', index),
+    path('courses/', include('requestApp.urls'))
+    #path('blog/', include('requestApp.urls'))
 ]
--- repetitor/requestApp/views.py
+++ repetitor/requestApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
-
+def info(request):
+    return render(request, 'requestApp/info_course.html')
 
 # Create your views here.
