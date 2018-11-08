8703d5fe596cb2529d0728040a8d89add7e3f429
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-11-08 23:24:17 +0300

    update Student, Course

--- repetitor/requestApp/models.py
+++ repetitor/requestApp/models.py
@@ -1,18 +1,24 @@
 from django.db import models
+# from django.contrib.auth import get_user_model
+
+# User = get_user_model()
 # from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Course(models.Model):
-    course_name = models.CharField(max_length = 150)
+    course_name = models.CharField('course_name', max_length = 150)
 
     def __str__(self):
         return self.course_name
 
 class Student(models.Model):
-    courses = models.ForeignKey(Course, on_delete="CASCADE")
-    name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    phone = models.CharField(max_length=12)
-    # phone = models.PhoneNumberField(null=False, blank=False, unique=True)
-    date_pub = models.DateTimeField(auto_now_add=True)
-    email = models.EmailField()
+    courses = models.ForeignKey(Course, verbose_name="Course",
+                                on_delete=models.CASCADE)
+    name = models.CharField('name', max_length=150)
+    last_name = models.CharField('last_name', max_length=150)
+    phone = models.CharField('phone', max_length=12)
+    date_pub = models.DateTimeField('date_pub', auto_now_add=True)
+    email = models.EmailField('email')
+
+    def __str__(self):
+        return 'Имя {} Фамилия {}'.format(self.name, self.last_name)
